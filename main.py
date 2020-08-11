ae73d38f7ff287abe9e1305713653791e6454b80
PhoenixizFire <edwyn.beauvery@gmail.com>
2020-08-11 17:21:00 +0200

    third commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
+@app.route("/deuxieme_index_trop_long_a_taper_pour_le_test")
 def home():
     return render_template("home.html")
 
@@ -10,9 +12,13 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/bug")
+def bug():
+    return render_templ("bug.html")
+
 @app.route("/salvador")
 def salvador():
-    return "Hello, Salvador"
+    return render_template("salvador.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
